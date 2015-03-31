982dab09dd720329c26d78a2aefc849e383a1ae8
al <pressl.alex@imitrix.de>
2015-03-31 02:56:56 +0200

    runfile

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
 rm m2mdali.tar
-tar -cf m m2mdali.tar m2m-lightcontrol.js node_modules/m2m-dali/ node_modules/m2m-socket-pipe node_modules/async/
+tar -cf m2mdali.tar run-light.sh m2m-lightcontrol.js node_modules/m2m-dali/ node_modules/m2m-socket-pipe node_modules/async/
 scp m2mdali.tar root@192.168.0.10:~
 
